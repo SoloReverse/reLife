da18420f76d854e009fd74bb59e299395f46b62f
SoloReverse <abdullah.nayssi@gmail.com>
2024-08-29 15:36:27 +0400

    changed to cookie based

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,13 +1,11 @@
-import { notFound } from "next/navigation";
 import { getRequestConfig } from "next-intl/server";
+import { getUserLocale } from "./services/locale";
 
-const locales = ["en", "ar"];
-
-export default getRequestConfig(async ({ locale }) => {
-  // Validate that the incoming `locale` parameter is valid
-  if (!locales.includes(locale as string)) notFound();
+export default getRequestConfig(async () => {
+  const locale = await getUserLocale();
 
   return {
+    locale,
     messages: (await import(`../i18n/${locale}.json`)).default,
   };
 });
