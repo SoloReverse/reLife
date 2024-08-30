fe8b08c0244f8313d196bbf387b1aa78462f9aa8
SoloReverse <abdullah.nayssi@gmail.com>
2024-08-30 14:04:25 +0400

    removed unused routes

--- src/app/api/assistants/threads/route.ts
+++
@@ -1,7 +0,0 @@
-// import { NextResponse } from "next/server";
-// import OpenAI from "openai";
-// const openai = new OpenAI();
-
-// export async function DELETE(request: Request) {}
-
-// export async function GET(request: Request) {}
