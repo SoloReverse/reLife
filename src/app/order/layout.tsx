550e6dbc42482f693e9e0384666a4aefa3bea7de
SoloReverse <abdullah.nayssi@gmail.com>
2024-09-02 09:52:35 +0400

    remove bg dev accident

--- src/app/order/layout.tsx
+++ src/app/order/layout.tsx
@@ -10,7 +10,7 @@ export default function Layout({
 }) {
   return (
     <div className="flex flex-col items-center justify-between w-full h-full">
-      <Card className="border-none w-full h-[95%] lg:max-w-[900px]rounded-2xl transition-all lg:max-w-[700px] lg:py-2 shadow-none lg:drop-shadow mx-48 lg:my-6 bg-neutral-600/50">
+      <Card className="border-none w-full h-[95%] lg:max-w-[900px]rounded-2xl transition-all lg:max-w-[700px] lg:py-2 shadow-none lg:drop-shadow mx-48 lg:my-6">
         {chatpage}
       </Card>
       <div>{children}</div>
